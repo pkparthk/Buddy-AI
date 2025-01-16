d3f02051dc3b3e656d026ecac6705febefa2a140
pkparthk <parthkothari75@gmail.com>
2025-01-17 02:27:38 +0530

    Update Frontend

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env": {}, // Define 'process.env' for compatibility
+  },
 });
